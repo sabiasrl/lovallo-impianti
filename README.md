9ec6ba2d8cf5f4d12588b485f634eb7e3eb23b17
SabiaSRL <daniele.sabia.dev@hotmail.com>
2026-05-08 13:22:05 +0200

    offline

--- README.md
+++
@@ -1,1 +0,0 @@
-# lovallo-impianti
--- script-loader.js
+++
@@ -1,5 +0,0 @@
-const isMobile = window.matchMedia("(max-width: 767px)").matches;
-const script = document.createElement("script");
-
-script.src = isMobile ? "mobile.js" : "desktop.js";
-document.body.appendChild(script);
